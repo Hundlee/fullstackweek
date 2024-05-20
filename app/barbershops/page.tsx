cd5fa6044d4cb876eb72f76b41bccaba4ad8519e
Gabriel Mesquita <Mesquitag884@gmail.com>
2024-05-20 19:18:46 -0300

    style: add responsive styles

--- app/barbershops/page.tsx
+++ app/barbershops/page.tsx
@@ -28,18 +28,20 @@ const BarbershopsPage = async ({ searchParams }: BarbershopsPageProps) => {
         <>
             <Header />
 
-            <div className="px-5 py-6 flex flex-col gap-6">
-                <Search
-                    defaultValues={{
-                        search: searchParams.search,
-                    }}
-                />
+            <div className="px-5 py-6 flex flex-col gap-6 md:px-10 xl:px-32">
+                <div className="lg:max-w-[50vw]">
+                    <Search
+                        defaultValues={{
+                            search: searchParams.search,
+                        }}
+                    />
+                </div>
 
                 <h1 className="font-bold text-gray-400 text-xs uppercase">
                     Resultados para &quot;{searchParams.search}&quot;
                 </h1>
 
-                <div className="grid grid-cols-2 gap-4">
+                <div className="grid grid-cols-2 gap-4 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5">
                     {barbershops.map((barbershop: any) => (
                         <div key={barbershop.id} className="w-full">
                             <BarbershopItem barbershop={barbershop} />
