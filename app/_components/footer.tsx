b08cd8471ad303d4ca50abf52e4977ddf3374b56
Gabriel Mesquita <Mesquitag884@gmail.com>
2024-05-19 23:30:53 -0300

    feat: add footer new description

--- app/_components/footer.tsx
+++ app/_components/footer.tsx
@@ -2,7 +2,7 @@ const Footer = () => {
     return (
         <footer className="w-full bg-[hsl(228,6%,16%)] py-6 px-5">
             <p className="text-gray-400 text-xs font-bold opacity-75">
-                © 2023 Copyright FSW Barber
+                © Hundlee 2023
             </p>
         </footer>
     );
