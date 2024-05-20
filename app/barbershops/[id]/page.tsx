c9a9e03d13efdac8be5d37a5d149b60b80019ba8
Gabriel Mesquita <Mesquitag884@gmail.com>
2024-05-20 18:55:43 -0300

    feat: Improvements to the barbershop details page with responsive layout and header
    - Adds the Header component to display the header on the barbershop details page.
    - Implements the BarbershopDetails component to display detailed barbershop information on larger screens.
    - Adds redirection to "notFound" page when barbershop ID is not found or not provided.
    - Adjusts the display of barbershop services to be presented in a grid on larger screens.
    - Improves page structure and organization for better user experience on different screen sizes.
    - Removes unnecessary comments and redirect TODOs.

--- app/barbershops/[id]/page.tsx
+++ app/barbershops/[id]/page.tsx
@@ -3,6 +3,9 @@ import BarbershopInfo from "./_components/barbershop-info";
 import ServiceItem from "./_components/service-item";
 import { getServerSession } from "next-auth";
 import { authOptions } from "@/app/_lib/auth";
+import Header from "@/app/_components/header";
+import BarbershopDetails from "./_components/barbershop-details";
+import { notFound } from "next/navigation";
 
 interface BarbershopDetailsPageProps {
     params: {
@@ -16,9 +19,7 @@ const BarbershopDetailsPage = async ({
     const session = await getServerSession(authOptions);
 
     if (!params.id) {
-        // TODO: redirecionar para home page
-        return null;
-        ("");
+        return notFound();
     }
 
     const barbershop = await db.barberShop.findUnique({
@@ -31,23 +32,36 @@ const BarbershopDetailsPage = async ({
     });
 
     if (!barbershop) {
-        // TODO: redirecionar para home page
-        return null;
+        return notFound();
     }
 
     return (
         <div>
-            <BarbershopInfo barbershop={barbershop} />
+            <div className="hidden lg:block">
+                <Header />
+            </div>
 
-            <div className="px-5 flex flex-col gap-4 py-6">
-                {barbershop.services.map((service: any) => (
-                    <ServiceItem
-                        key={service.id}
-                        barbershop={barbershop}
-                        service={service}
-                        isAuthenticated={!!session?.user}
-                    />
-                ))}
+            <div className="flex lg:px-10 2xl:px-32 justify-between lg:gap-8 lg:pt-8">
+                <div className="w-full lg:min-w-[60vw]">
+                    <BarbershopInfo barbershop={barbershop} />
+                    <div className="mt-6 px-5 md:px-10 lg:px-0">
+                        <h2>Serviços</h2>
+                    </div>
+                    <div className="flex px-5 lg:px-0  md:px-10 flex-col gap-4 py-6 lg:grid lg:grid-cols-1 2xl:grid-cols-2">
+                        {barbershop.services.map((service: any) => (
+                            <ServiceItem
+                                key={service.id}
+                                barbershop={barbershop}
+                                service={service}
+                                isAuthenticated={!!session?.user}
+                            />
+                        ))}
+                    </div>
+                </div>
+
+                <div className="w-full hidden lg:block ">
+                    <BarbershopDetails barbershop={barbershop} />
+                </div>
             </div>
         </div>
     );
