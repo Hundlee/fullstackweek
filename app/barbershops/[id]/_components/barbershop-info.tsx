77779dfb50fb256e2beefa0375dfd80926a845eb
Gabriel Mesquita <Mesquitag884@gmail.com>
2024-05-20 18:56:51 -0300

    feat: Improvements to the BarbershopInfo component with responsive layout
    - Added conditional display of elements based on screen size (lg:hidden and lg:block).
    - Introduces a new background image for large screens with rounded edges.
    - Adjusts the display of barbershop information for better organization on different screen sizes.
    - Added a flexible layout to display views and addresses on larger screens.
    - Keep the original layout for small screens and improve the code structure.

--- app/barbershops/[id]/_components/barbershop-info.tsx
+++ app/barbershops/[id]/_components/barbershop-info.tsx
@@ -21,7 +21,7 @@ const BarbershopInfo = ({ barbershop }: BarbershopInfoProps) => {
 
     return (
         <div>
-            <div className="h-[250px] w-full relative">
+            <div className="h-[250px] w-full relative lg:hidden">
                 <Button
                     variant="outline"
                     size="icon"
@@ -51,20 +51,52 @@ const BarbershopInfo = ({ barbershop }: BarbershopInfoProps) => {
                     src={barbershop.imageUrl}
                     fill
                     alt={barbershop.name}
-                    style={{ objectFit: "cover" }}
-                    className="opacity-85"
+                    className="opacity-85 object-cover"
                 />
             </div>
 
-            <div className="px-5 pt-3 pb-6 border-b border-solid border-secondary">
-                <h1 className="text-xl font-bold">{barbershop.name}</h1>
-                <div className="flex items-center gap-1 mt-2">
-                    <MapPinIcon className="text-primary" size={18} />
-                    <p className="text-sm">{barbershop.address}</p>
+            <div>
+                <div className="hidden lg:block w-full h-[650px] relative">
+                    <Image
+                        src="/barbershop.jpg"
+                        fill
+                        alt={barbershop.name}
+                        className="object-cover lg:rounded-lg"
+                    />
                 </div>
-                <div className="flex items-center gap-1 mt-2">
-                    <StarIcon className="text-primary" size={18} />
-                    <p className="text-sm">5,0 (389 avaliações)</p>
+
+                <div className="px-5 md:px-10 pt-3 pb-6 border-b border-solid border-secondary lg:px-0 lg:hidden">
+                    <h1 className="text-xl font-bold">{barbershop.name}</h1>
+                    <div className="flex items-center gap-1 mt-2">
+                        <MapPinIcon className="text-primary" size={18} />
+                        <p className="text-sm">{barbershop.address}</p>
+                    </div>
+                    <div className="flex items-center gap-1 mt-2">
+                        <StarIcon className="text-primary" size={18} />
+                        <p className="text-sm">5,0 (389 avaliações)</p>
+                    </div>
+                </div>
+
+                <div className="px-5 md:px-10 pt-3 pb-6 border-b border-solid border-secondary lg:px-0 lg:flex lg:justify-between">
+                    <div>
+                        <h1 className="text-2xl font-bold">
+                            {barbershop.name}
+                        </h1>
+                        <div className="flex items-center gap-1 mt-2">
+                            <MapPinIcon className="text-primary" size={18} />
+                            <p className="text-sm">{barbershop.address}</p>
+                        </div>
+                    </div>
+                    <div className="flex flex-col items-center gap-1 mt-2 bg-secondary rounded-lg p-5">
+                        <div className="text-xl flex items-center gap-1">
+                            <StarIcon
+                                className="text-primary fill-primary"
+                                size={18}
+                            />
+                            5,0
+                        </div>
+                        <span>389 avaliações</span>
+                    </div>
                 </div>
             </div>
         </div>
