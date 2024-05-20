16e5cfac550e7377ee73fdefa61b5d4726549d7d
Gabriel Mesquita <Mesquitag884@gmail.com>
2024-05-20 19:18:57 -0300

    style: add responsive styles

--- app/_components/footer.tsx
+++ app/_components/footer.tsx
@@ -1,6 +1,6 @@
 const Footer = () => {
     return (
-        <footer className="w-full bg-[hsl(228,6%,16%)] py-6 px-5">
+        <footer className="w-full bg-[hsl(228,6%,16%)] py-6 px-5 md:px-10 lg:px-10 xl:px-32">
             <p className="text-gray-400 text-xs font-bold opacity-75">
                 © Hundlee 2023
             </p>
